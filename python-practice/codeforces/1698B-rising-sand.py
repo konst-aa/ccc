6ddb2e3aef3fcd6c9bcf9e5ef9336c97cf1447a9
Konstantin Astafurov <40547702+konstantin-aa@users.noreply.github.com>
2022-07-09 00:14:03 -0400

    continue is something you can do in python. the more you know

--- python-practice/codeforces/1698B-rising-sand.py
+++ python-practice/codeforces/1698B-rising-sand.py
@@ -20,21 +20,22 @@ def insr():
 def invr():
     return map(int, input().split())
 
+
 # solving: https://codeforces.com/contest/1698/problem/B
+
 
 def solve():
     num_tests = inp()
-    ans = []
     for x in range(num_tests):
         num_piles, operations = inlt()
         sizes = inlt()
         if operations == 1:
-            ans += [str((num_piles - 1) // 2)]
-        else:
-            count = 0
-            for i in range(1, num_piles - 1):
-                count += 1 if sizes[i] > sizes[i-1] + sizes[i+1] else 0
-            ans += [str(count)]
-    print("\n".join(ans))
+            print((num_piles - 1) // 2)
+            continue
+        count = 0
+        for i in range(1, num_piles - 1):
+            count += 1 if sizes[i] > sizes[i - 1] + sizes[i + 1] else 0
+        print(count)
+
 
 solve()
