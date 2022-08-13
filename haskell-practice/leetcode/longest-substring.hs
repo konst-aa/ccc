0a2be1108c204ebf3d83d1277ae4193ce0c39f94
Konstantin Astafurov <40547702+konstantin-aa@users.noreply.github.com>
2022-08-12 22:39:43 -0400

    nvm i gotta think more

--- haskell-practice/leetcode/longest-substring.hs
+++ haskell-practice/leetcode/longest-substring.hs
@@ -2,12 +2,12 @@ import qualified Data.Set as Set
 
 folder :: (Set.Set Char, Int) -> Char -> (Set.Set Char, Int)
 folder (seen, best) char
-    | Set.member char seen = (Set.empty, best)
+    | Set.member char seen = (Set.delete char seen, best)
     | Set.size seen + 1 > best = (Set.insert char seen, best + 1)
     | otherwise = (Set.insert char seen, best) 
 
 -- solving: https://leetcode.com/problems/longest-substring-without-repeating-characters/
 main :: IO()
 main = do 
-    let (_, best) = Prelude.foldl folder (Set.empty, 0) "abcabcbb"
+    let (_, best) = Prelude.foldl folder (Set.empty, 0) "abacde"
     print best
