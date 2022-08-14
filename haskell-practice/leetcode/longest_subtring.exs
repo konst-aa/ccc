4c08898fc536d89e7c075f0693dd71a2cce6959d
Konstantin Astafurov <40547702+konstantin-aa@users.noreply.github.com>
2022-08-14 00:24:25 -0400

    cheeky elixir solution along in there, fell back on it when didnt work

---
+++ haskell-practice/leetcode/longest_subtring.exs
@@ -0,0 +1,20 @@
+defmodule Solution do
+  @spec length_of_longest_substring(s :: String.t()) :: integer
+  def length_of_longest_substring(s) do
+    input_chars = String.to_charlist(s)
+    {_, _, best, _} = Enum.reduce(input_chars, {%{}, 0, 0, 0}, &reduce_fn/2)
+    best
+  end
+
+  def reduce_fn(char, {positions, past, best, index}) do
+    new_index = index + 1
+    val = Map.get(positions, char, 0)
+    new_past = max(val, past)
+    new_best = max(new_index - new_past, best)
+    {Map.put(positions, char, new_index), new_past, new_best, new_index}
+  end
+end
+
+"aab"
+|> Solution.length_of_longest_substring()
+|> IO.inspect()
